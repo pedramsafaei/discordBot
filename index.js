0a109efbd16c6eb70e97d39f9b6a30e491e6c278
Pedram Safaei <psafaei@nevada.unr.edu>
2020-04-16 17:53:30 -0700

    bot can kick members now

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const discord = require("discord.js");
 const client = new discord.Client();
-const { prefix, token } = require("./config.json");
+const { prefix, token, giphyToken } = require("./config.json");
+
+var GphApiClient = require("giphy-js-sdk-core");
+client = GphApiClient(giphyToken);
 
 client.once("ready", () => {
   console.log("Ready!");
 });
+
+client.on("message", (message) => {
+  if (message.member.hasPermission(["KICK_MEMBERS", "BAN_MEMBERS"])) {
+    if (message.content.startsWith(`${prefix}kick`)) {
+      //console.log(message.content);
+      //message.channel.send("Kick");
+      let member = message.mentions.members.first();
+      member.kick().then((member) => {
+        message.channel.send(
+          ":wave: " + member.displayName + " has been kicked!"
+        );
+      });
+    }
+  }
+});
+
 client.login(token);
