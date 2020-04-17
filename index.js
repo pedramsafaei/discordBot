507ad81e94939d9e4cc2249a4949a587d32b4324
Pedram Safaei <psafaei@nevada.unr.edu>
2020-04-17 01:17:04 -0700

    you can now search for gifs

--- index.js
+++ index.js
@@ -10,10 +10,9 @@ client.once("ready", () => {
 });
 
 client.on("message", (message) => {
+  //KICK MEMBERS
   if (message.member.hasPermission(["KICK_MEMBERS", "BAN_MEMBERS"])) {
     if (message.content.startsWith(`${prefix}kick`)) {
-      //console.log(message.content);
-      //message.channel.send("Kick");
       let member = message.mentions.members.first();
       member.kick().then((member) => {
         giphy
@@ -32,13 +31,35 @@ client.on("message", (message) => {
                 files: [responseFinal.images.fixed_height.url],
               }
             );
+            console.log(files);
           })
           .catch(() => {
-            message.channel.send("Error!!!");
+            message.channel.send("Can't kick out the fucking user!!!");
           });
       });
     }
   }
+  //FIND GIFS
+  if (message.member.hasPermission(["SEND_MESSAGES"])) {
+    if (message.content.startsWith(`${prefix}gif`)) {
+      //let member = message.mentions.members.first();
+      var actualgif = message.content.split("gif").pop();
+      actualgif = actualgif.replace(/\s/g, "");
+      giphy
+        .search("gifs", { q: actualgif })
+        .then((response) => {
+          var totalResponses = response.data.length;
+          var responseIndex =
+            Math.floor(Math.random() * 10 + 1) % totalResponses;
+          var responseFinal = response.data[responseIndex];
+          message.channel.send({
+            files: [responseFinal.images.fixed_height.url],
+          });
+        })
+        .catch(() => {
+          message.channel.send("Can't find this gif you FUCKER!!!");
+        });
+    }
+  }
 });
-
 client.login(token);
