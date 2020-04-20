fea327a08c7f25549a62794123d5c7b32701c4e2
Pedram Safaei <psafaei@nevada.unr.edu>
2020-04-20 13:29:52 -0700

    unsuccessful, try later

--- index.js
+++ index.js
@@ -71,5 +71,43 @@ client.on("message", (message) => {
       } catch (err) {}
     }
   }
+  if (message.content.startsWith(`${prefix}spotify`)) {
+    if (message.member.hasPermission(["SEND_MESSAGES"])) {
+      let user = message.mentions.users.first() || message.author;
+      if (
+        user.presence.game !== null &&
+        user.presence.game.type === 2 &&
+        user.presence.game.name == "Spotify" &&
+        user.presence.game.assets !== null
+      ) {
+        let trackIMG = `https://i.scdn.co/image/${user.presence.game.assets.largeImage.slice(
+          8
+        )}`;
+        let trackURL = `https://open.spotify.com/track/${user.presence.game.syncID}`;
+        let trackAuthor = user.presence.game.details;
+        let trackAlbum = user.presence.game.assets.largeText;
+        const embed = new discord.MessageEmbed()
+          .setAuthor(
+            "Spotify track info",
+            "https://cdn.discordapp.com/emojis/408668371039682560.png"
+          )
+          .setColor(0x1ed760)
+          .setThumbnail(trackIMG)
+          .addField("Song Name", trackName, true)
+          .addField("Album", trackAlbum, true)
+          .addField("Author", trackAuthor, false)
+          .addField(
+            "listening to track:",
+            `[\`${trackURL}\`](trackURL)`,
+            false
+          );
+
+        message.channel.send(embed);
+      } else {
+        message.channel.send("This user is not listening to spotify!");
+      }
+    }
+  }
 });
+
 client.login(token);
